3f692dde31d340c39e34d542fc70bbb5fbd9de81
Kazuki Ohta <ohtakazuki@gmail.com>
2017-09-19 04:56:35 +0000

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    # ルートページ
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    
+    # contact
+    get contact_path
+    assert_select "title", full_title("Contact")
+
+    # サインアップ
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")
+  end
+end
